05371f1e9c3082e8f8610ab74c607e93aa02beae
Ozan Andaç <oynozan@hotmail.com>
2026-08-14 10:19:13 +0200

    docs: gamemodes section with per-game guides and integration

--- docs/content/_meta.js
+++ docs/content/_meta.js
@@ -2,6 +2,7 @@ export default {
     index: "What is Foreseer",
     "how-it-works": "How it works",
     rules: "Game rules",
+    gamemodes: "Gamemodes",
     verify: "Verify your bet",
     sdk: "SDK",
     api: "Server API",
---
+++ docs/content/gamemodes/_meta.js
@@ -0,0 +1,7 @@
+export default {
+    "european-roulette": "European roulette",
+    "american-roulette": "American roulette",
+    dice: "Dice",
+    coinflip: "Coinflip",
+    towers: "Towers",
+};
